fa0797e9ca8c10a66382dcb4ba70901205619dae
Ramesh Anandh <rameshdev55@gmail.com>
2016-11-11 12:56:10 +0530

    v1

---
+++ Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="Home Page -- Service Provider" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+ 
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <center>
+    <table>
+    <tr>
+    <td class="style2" colspan="2">
+    
+    <h2>
+        Welcome Service Provider!
+    </h2>
+    
+    </td>
+    </tr>
+    <tr>
+    <td class="style2">
+    <h2>
+    FILTER DATA
+    </h2>
+        </td>
+    <td class="style3">
+    
+        <asp:TextBox ID="TextBox1" runat="server" Width="198px" AutoPostBack="True" 
+            ontextchanged="TextBox1_TextChanged"></asp:TextBox>
+    
+    </td>
+    </tr>
+    <tr>
+    <td class="style2" colspan="2">
+    
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+    
+        </td>
+    </tr>
+    <tr>
+    <td class="style2" colspan="2">
+    <div id="dvMap" style="width: 500px; height: 500px">
+</div>
+        &nbsp;</td>
+    </tr>
+    </table>
+    </center>
+    
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
+using MySql.Data;
+public partial class _Default : System.Web.UI.Page
+{
+    MySqlConnection c1 = new MySqlConnection("server=b1cd7557-6515-4315-b173-a41000718d78.mysql.sequelizer.com;User Id=qhjmurvgkjyckfli;Password=igJW4areBPSwLjbX3JVDAbLwAKKBhG5z3mME5hFL8sai7ALovjkWz73gCnurwW53;Persist Security Info=True;database=dbb1cd755765154315b173a41000718d78");
+    public void bindgrid(string rr)
+    {
+         try
+        {
+        DataSet dt = new DataSet();
+        c1.Open();
+        MySqlDataAdapter da = new MySqlDataAdapter(rr, c1);
+        da.Fill(dt);
+        GridView1.DataSource = dt.Tables[0];
+        GridView1.DataBind(); da.Dispose();        
+        }
+         catch (Exception er)
+         {
+         }
+         finally
+         {
+             c1.Close();
+         }
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {           
+        bindgrid("select * from sample");
+        TextBox1.Focus();
+    }
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+        if (TextBox1.Text != "*")
+        {
+            bindgrid("select * from sample where Mobile='" + TextBox1.Text + "'");
+        }
+        else
+        {
+            bindgrid("select * from sample");
+        }
+    }
+}
